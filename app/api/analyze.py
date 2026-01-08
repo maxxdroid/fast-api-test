77255482bcbc074aba7a6af038dd53b040135e8d
maxxdroid <theonlymaxmc@gmail.com>
2026-01-08 12:34:31 +0000

    implement analyze api

--- app/api/analyze.py
+++ app/api/analyze.py
@@ -1,7 +1,19 @@
 from fastapi import APIRouter
+from pydantic import BaseModel
+from app.services.analyze_service import analyze_image
+from app.services.upload_service import get_image_path
+
 
 router = APIRouter()
 
-@router.get("/analyze")
-def get_upload():
-    return "Hola Mf"
+class AnalyzeRequest(BaseModel):
+    image_id: str
+
+
+@router.post("/analyze")
+def analyze_image_endpoint(request: AnalyzeRequest):
+    # Check if image exists
+    get_image_path(request.image_id)
+    # Run  analysis
+    result = analyze_image(request.image_id)
+    return result
